1c015bfa7ddcaf6d5da9ded62c5a96eca98e538b
heera9331 <heera9331@gmail.com>
2024-11-15 22:44:46 +0530

    lib prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+const prismaConfig = {
+  log: ["query", "info", "warn", "error"],
+};
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query", "info", "warn", "error"],
-  });
+// Reuse Prisma Client if already initialized, otherwise create a new one.
+const prisma = globalForPrisma.prisma || new PrismaClient(prismaConfig);
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
